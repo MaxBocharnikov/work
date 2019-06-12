63ed0d460268be432d573e58cc4323b9d457bd37
MaxBocharnikov <bocharnikov3009@gmail.com>
2019-06-12 18:28:08 +0700

    Закончил работу

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,11 @@ function showModal () {
 
 function hideModal() {
     $('.modal--create').removeClass('show');
+    $('.modal__error').text('');
+}
+
+function showModalError() {
+    $('.modal__error').addClass('show');
 }
 
 $('.hamburger').on('click', function() {
@@ -21,10 +26,38 @@ $('.modal__close').on('click', function () {
 	hideModal();
 });
 
+$('.modal__button--cancel').on('click', function (e) {
+	e.preventDefault();
+    hideModal();
+});
+
 $(document).keyup(function(e) {
     if (e.keyCode === 27) {
        	if($('.modal--create').hasClass('show')) {
        		hideModal();
 		}
     }
+});
+
+$('.modal__form').submit(function (e) {
+	e.preventDefault();
+	var domain = $('.modal__input').val();
+	if(domain.match(/^[a-zA-Z\-]+$/)) {
+		hideModal();
+		alert('Данные отправлены');
+	} else {
+		var message;
+		if (!domain) {
+			message = 'Домен не может быть пустым';
+		} else if (domain.match('work5')) {
+            message = 'Запрещается использовать словосочетание Work 5';
+		}
+		else {
+            var message = 'Домен может содержать только латинские символы, а также тире';
+		}
+
+		$('.modal__error').text(message);
+		showModalError();
+	}
+
 });
